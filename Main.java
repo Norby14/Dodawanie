9dedd441afc761ba817ede9dcdba12ae94b0b8dc
Norby141 <100698239+Norby14@users.noreply.github.com>
2022-03-12 11:14:39 +0000

    kalkulator

--- Main.java
+++ Main.java
@@ -1,5 +1,59 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  } 
-}				         
+import java.util.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Wpisz pierwszą liczbę:");
+        Scanner pobierz = new Scanner(System.in);
+        int liczba1,liczba2;
+        liczba1 = pobierz.nextInt();
+				System.out.println("Wpisz drugą liczbę:");
+        liczba2 = pobierz.nextInt();
+        int ans;
+        System.out.println("Wybierz operacje:\n 1 - Dodawanie \n 2 - Odejmowanie \n 3 - Mnożenie \n 4 - Dzielenie");
+        int wybierz;
+        wybierz = pobierz.nextInt();
+        switch (wybierz){
+        case 1:
+            System.out.println(dodawanie( liczba1,liczba2));
+            break;
+        case 2:
+            System.out.println(odejmowanie( liczba1,liczba2));
+            break;      
+        case 3:
+            System.out.println(mnozenie( liczba1,liczba2));
+            break;
+        case 4:
+            System.out.println(dzielenie( liczba1,liczba2));
+            break;
+            default:
+                System.out.println("Illigal Operation");
+
+
+        }
+
+
+
+    }
+    public static int dodawanie(int x, int y)
+    {
+        int wynik = x + y;
+        return wynik;
+    }
+    public static int odejmowanie(int x, int y)
+    {
+        int wynik = x-y;
+        return wynik;
+    }
+    public static int mnozenie(int x, int y)
+    {
+        int wynik = x*y;
+        return wynik;
+    }
+    public static int dzielenie(int x, int y)
+    {
+        int wynik = x/y;
+        return wynik;
+    }
+
+}
